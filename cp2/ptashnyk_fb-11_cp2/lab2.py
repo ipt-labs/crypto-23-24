4b865450556994c0d315de6897f13dbe04590d1d
Yurii Ptashnyk <ptisa823@gmail.com>
2023-10-14 15:18:01 +0300

    Lab2 final

--- cp2/ptashnyk_fb-11_cp2/lab2.py
+++ cp2/ptashnyk_fb-11_cp2/lab2.py
@@ -18,10 +18,14 @@ def VigenereEncrypt(text,key,alphabet):
         encryptedText += alphabet[encryptedCharIndex]
     return encryptedText
 
-def SplitTextIntoBlocks(text,numberOfBlocks):
+def SplitTextIntoBlocks(text,numberOfBlocks = 0,numberOfChars = 0):
     blocks = []
-    for i in range(0,numberOfBlocks):
-        blocks.append(text[i::numberOfBlocks])
+    if numberOfBlocks:
+        for i in range(0,numberOfBlocks):
+            blocks.append(text[i::numberOfBlocks])
+    elif numberOfChars:
+        for i in range(0,len(text),numberOfChars):
+            blocks.append(text[i:i + numberOfChars])
     return blocks
 
 def CountLettersEntries(text,alphabet):
@@ -55,48 +59,92 @@ def TheoreticalConformityIndex(filename):
         theoreticalConformityIndex += float(letterData[2]) ** 2
     return theoreticalConformityIndex
 
-def GenerateCSV(filename,data):
+def GenerateCSV(filename,data,header):
     with open(f"{filename}.csv", "w", newline = "") as file:
         csvwriter = writer(file)
+        csvwriter.writerow(header)
         csvwriter.writerows(data)
 
-def FindEncryptionKey(textToDecrypt,fileWithData):
-    decryptionIndex = {}
+def DecryptionIndex(textToDecrypt,alphabet):
+    decryptionIndex = []
     for r in range(2,31):
-        blocks = SplitTextIntoBlocks(textToDecrypt,r)
-        decryptionIndex.update({r:BlocksConformityIndex(blocks,decryptionAlphabet)})
+        blocks = SplitTextIntoBlocks(textToDecrypt,numberOfBlocks = r)
+        decryptionIndex.append([r,BlocksConformityIndex(blocks,alphabet)])
+    return decryptionIndex
+
+def FindPossibleKeys(decryptionIndex,fileWithData):
     possibleKeys = []
+    Keys = []
     theoreticalIndex = TheoreticalConformityIndex(fileWithData)
-    for key, value in decryptionIndex.items():
-        if abs(theoreticalIndex - value) <= 0.01:
-            possibleKeys.append(key)
-    return possibleKeys
+    for i in decryptionIndex:
+        if abs(theoreticalIndex - i[1]) <= 0.01:
+            possibleKeys.append(i[0])
+    for i in range(0,len(possibleKeys)):
+        for j in range(i + 1,len(possibleKeys)):
+            if (possibleKeys[j] % possibleKeys[i] == 0) and (possibleKeys[i] not in Keys):
+                Keys.append(possibleKeys[i])
+    return Keys
 
-def VigenereDecrypt(encryptedText,key,alphabet):
-    entries = CountLettersEntries(encryptedText,alphabet)
-    blocks = SplitTextIntoBlocks(encryptedText,len(encryptedText) // key)
-    print(blocks)
+def BlocksOfCeasar(encryptedText,key):
+    blocksOfCeasar = []
+    block = ''
+    blocks = SplitTextIntoBlocks(encryptedText,numberOfChars = key)
+    for i in range(0,key):
+        for j in blocks:
+            block += j[i]
+        blocksOfCeasar.append(block)
+        block = ''
+    return blocksOfCeasar
 
+def FindCeasarKey(text,alphabet):
+    entries = CountLettersEntries(text,alphabet)
+    mostFrequentLetter = max(entries, key = entries.get)
+    key = (alphabet.find(mostFrequentLetter) - alphabet.find('о')) % len(alphabet)
+    return key
+
+def VigenereDecrypt(text,key,alphabet):
+    keyCharIndex = 0
+    decryptedText = ''
+    for char in text:
+        decryptedCharIndex = (alphabet.find(char) - alphabet.find(key[keyCharIndex])) % len(alphabet)
+        keyCharIndex = (keyCharIndex + 1) % len(key)
+        decryptedText += alphabet[decryptedCharIndex]
+    return decryptedText
 
 if __name__ == "__main__":
+
+    #Task1 and Task2
+
     keys = ["об","нам","круг","мышка","ассоциация","антикоммунистический"]
-    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-    decryptionAlphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+    alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+
     text = ReadFile("textToEncrypt.txt")
     text = ClearText(text,alphabet)
-    encryptionIndex = [["r value", "Index"]]
+    encryptionIndex = []
     for key in keys:
         if len(key) <= len(text):
             encryptedText = VigenereEncrypt(text,key,alphabet)
-            with open(f"Encrypted with period {len(key)}.txt", "w") as file:
+            with open(f"EncryptedWithPeriod{len(key)}.txt", "w") as file:
                 file.write(encryptedText)
             encryptionIndex.append([len(key),ConformityIndex(encryptedText,alphabet)])
         else:
             print("Encryption key length must be equal or shorter than text!")
     encryptionIndex.append(["Clear text", ConformityIndex(text,alphabet)])
-    GenerateCSV("Encrypted Index", encryptionIndex)
+    GenerateCSV("EncryptedIndex", encryptionIndex, ["r value", "Index"])
+
+    #Task3
 
     textToDecrypt = ReadFile("textToDecrypt.txt")
-    textToDecrypt = ClearText(textToDecrypt,decryptionAlphabet)
-    possibleKeys = FindEncryptionKey(textToDecrypt,"OnlyLetters.csv")
-    VigenereDecrypt(textToDecrypt,possibleKeys[0],decryptionAlphabet)
+    textToDecrypt = ClearText(textToDecrypt,alphabet)
+    decryptionIndex = DecryptionIndex(textToDecrypt,alphabet)
+    GenerateCSV("DecryptionIndex",decryptionIndex,["r value","Index"])
+    possibleKeys = FindPossibleKeys(decryptionIndex,"OnlyLetters.csv")
+    blocksOfCeasar = BlocksOfCeasar(textToDecrypt,possibleKeys[0])
+    vigenereKey = ''
+    for block in blocksOfCeasar:
+        key = FindCeasarKey(block,alphabet)
+        vigenereKey += alphabet[key]
+    vigenereKey = 'чугунныенебеса' #чкгунныенебеиа
+    decryptedText = VigenereDecrypt(textToDecrypt,vigenereKey,alphabet)
+    with open("DecryptedText.txt","w") as file:
+        file.write(decryptedText)
